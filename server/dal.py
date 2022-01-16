9c3a2a4829eca37d6c235a1010fb78ee7570ea11
bvolkov <b.volkov@solarl.ru>
2022-01-16 12:38:28 +0300

    data access layer

---
+++ server/dal.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+
+class OwnClient:
+
+    def __init__(self):
+        self.mongoUrl = 'localhost'
+        self.client = MongoClient(self.mongoUrl)
+        pass
+
+    def addDataToCalculate(self, jsonData, userId=None):
+        db = self.client['UserStorage']
+        clt = db['Data']
+
+        data_id = clt.insert_one(jsonData).inserted_id
+        print(f'{data_id}')
+
+        if(userId==None):
+            userId = 'lol'
+
+        
+
+        return
+
+db = OwnClient()
